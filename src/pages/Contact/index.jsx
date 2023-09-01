73a4beadb667649b56270de15f5d7dca1087c6b1
kzmfhm <khuzaimanazik4@gmail.com>
2023-09-01 15:28:51 +0500

    Styling in pages

--- src/pages/Contact/index.jsx
+++ src/pages/Contact/index.jsx
@@ -1,12 +1,13 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 import Navbar from '../../components/Navbar';
 import WomenImg from '../../images/woman1.png';
 import AccountBoxIcon from '@mui/icons-material/AccountBox';
 import QuestionAnswerIcon from '@mui/icons-material/QuestionAnswer';
 import EmailIcon from '@mui/icons-material/Email';
 import ChatIcon from '@mui/icons-material/Chat';
-const Contact = () => {
+import './style.css';
 
+const Contact = () => {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [message, setMessage] = useState('');
@@ -14,8 +15,6 @@ const Contact = () => {
   const [responseMessage, setResponseMessage] = useState('');
   const [isLoading, setIsLoading] = useState(false); // Step 1
   const baseUrl = 'http://localhost:8000';
-
-
 
   const validateEmail = (email) => {
     const isValid = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
@@ -33,7 +32,7 @@ const Contact = () => {
       return;
     }
 
-    setIsLoading(true); 
+    setIsLoading(true);
 
     const dataSend = {
       name: name,
@@ -52,13 +51,13 @@ const Contact = () => {
       });
 
       if (res.status > 199 && res.status < 300) {
-        setResponseMessage('Message Send Successfully!');
+        setResponseMessage('Message Sent Successfully!');
       } else {
         setResponseMessage('Error sending email. Please try again.');
       }
     } catch (error) {
       console.error('Error sending email:', error);
-      setResponseMessage('Error sending email. Please try again.');
+      setResponseMessage('Error sending email.');
     } finally {
       setIsLoading(false); // Reset loading state
     }
@@ -72,57 +71,68 @@ const Contact = () => {
           <img src={WomenImg} alt='Woman' />
         </div>
         <div class='md:h-[700px] h-[700px] md:mt-[10px] mt-[20px] md:ml-10 md:w-[950px] w-[350px] ml-[20px] bg-[#111]'>
-        <p class='md:ml-[220px] h-[30px] md:mt-[40px] align-middle text-white text-2xl cursor-pointer transition ease-in-out delay-90 hover:text-yellow-600  hover:-translate-y-1 hover:scale-110 duration-100 ...'>
+          <p class='md:ml-[220px] h-[30px] md:mt-[40px] align-middle text-white text-2xl cursor-pointer transition ease-in-out delay-90 hover:text-yellow-600  hover:-translate-y-1 hover:scale-110 duration-100 ...'>
+           
             ________________________{' '}
             <span className='mt-[-20px]'>
-              <QuestionAnswerIcon style={{color: '#daa520' }} />
-            </span>{' '} ________________________
+              <QuestionAnswerIcon style={{ color: '#daa520' }} />
+            </span>{' '}
+            ________________________
           </p>
+          
           <p class='md:ml-[220px] mt-[30px] align-middle text-white text-1xl cursor-pointer transition ease-in-out delay-150 hover:text-yellow-600'>
-         
-          ⚡If you have any suggestion, project or even you want to say Hello..<br/> please fill out the form below and I will reply you shortly.
-          ⚡</p>
-        
+            ⚡If you have any suggestions, projects, or even want to say
+            Hello..<br /> please fill out the form below, and I will reply
+            shortly.⚡
+          </p>
+
           <form className='bg-[#000000]  md:ml-[220px] ml-[20px] w-[310px] h-[500px] md:h-[500px] md:w-[510px]'>
             <ul class='mt-[10px]'>
               <li className=''>
-                <label class='mx-4'> <AccountBoxIcon style={{color: '#daa520' }}/></label>
-                <input class='mt-[50px] mx-2 text-yellow-700 p-2 bg-[#111] h-[40px] md:w-[400px]'
-                  id="name"
-                  label="Enter your name.."
-                  variant="standard"
+                <label class='mx-4'>
+                  {' '}
+                  <AccountBoxIcon style={{ color: '#daa520' }} />
+                </label>
+                <input
+                  class='mt-[50px] mx-2 text-yellow-700 p-2 bg-[#111] h-[40px] md:w-[400px]'
+                  id='name'
+                  label='Enter your name..'
+                  variant='standard'
                   value={name}
                   onChange={(e) => setName(e.target.value)}
                   required
-                 
                 />
               </li>
               <li className=''>
-              <label class='mx-4'> <EmailIcon style={{color: '#daa520' }}/></label>
-                <input class='mt-[50px] mx-2 text-yellow-700 p-2 bg-[#111] h-[40px] md:w-[400px]'
-                  id="email"
-                  label="Enter your email.."
-                  variant="standard"
+                <label class='mx-4'>
+                  {' '}
+                  <EmailIcon style={{ color: '#daa520' }} />
+                </label>
+                <input
+                  class='mt-[50px] mx-2 text-yellow-700 p-2 bg-[#111] h-[40px] md:w-[400px]'
+                  id='email'
+                  label='Enter your email..'
+                  variant='standard'
                   value={email}
                   onChange={(e) => {
                     setEmail(e.target.value);
                     setEmailValidationMessage('');
                   }}
                   required
-                  InputProps={{
-                    style: {
-                      color: 'white', // Text color
-                      // Add other styles here
-                    },
-                  }}
                 />
                 {emailValidationMessage && (
                   <p className='error-message'>{emailValidationMessage}</p>
                 )}
               </li>
               <li>
-              <label class='mx-4'> <ChatIcon style={{color: '#daa520' }}/></label>
-                <textarea class='mt-[50px] mx-2 text-yellow-700 p-2 bg-[#111] h-[40px] md:w-[400px]'
+                <label class='mx-4'>
+                  {' '}
+                  <ChatIcon
+                    style={{ marginBottom: '70px', color: '#daa520' }}
+                  />
+                </label>
+                <textarea
+                  class='md:mt-[50px] mt-[50px] mx-2 text-yellow-700 p-2 bg-[#111] md:h-[80px] w-[200px] md:w-[400px]'
                   onChange={(e) => setMessage(e.target.value)}
                   name='message'
                   placeholder='Message'
@@ -131,26 +141,22 @@ const Contact = () => {
               <li>
                 {/* Conditional rendering based on isLoading */}
                 {isLoading ? (
-                  <button class='mt-[50px] mx-2 text-white bg-[#daa520] p-2  h-[40px] md:w-[400px]' disabled>
+                  <div className='loader-wrapper'>
                     <div className='loader'></div>
-                  </button>
+                  </div>
                 ) : (
                   <input
-                    className='md:mt-[50px] md:ml-[60px] text-[#000000] p-2 bg-[#111] h-[40px] md:w-[400px]'
+                    className='md:ml-[60px] ml-[60px] text-[#000000] p-2 w-[200px] bg-[#111] h-[40px] md:w-[400px]'
                     type='button'
                     value='Send'
                     onClick={sendEmail}
                     disabled={isLoading}
-                    style={{ backgroundColor: '#daa520' }} // Add this style
+                    style={{ backgroundColor: '#daa520' }}
                   />
                 )}
-               {responseMessage && !isLoading && (
-                
-                  <p className='mt-[50]' style={{ marginTop:'10px',marginLeft:'150px',color: '#daa520', fontSize: '18px' }}>
-                    {responseMessage}
-                  </p>
+                {responseMessage && !isLoading && (
+                  <p className='loading-tag'>{responseMessage}</p>
                 )}
-  
               </li>
             </ul>
           </form>
@@ -161,4 +167,3 @@ const Contact = () => {
 };
 
 export default Contact;
-
