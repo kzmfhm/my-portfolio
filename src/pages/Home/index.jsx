7b56c8b1736499f83c745c72b0cf7a8239646c32
kzmfhm <khuzaimanazik4@gmail.com>
2023-09-03 13:04:27 +0500

    Adding solar-system animation in home page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,18 +1,6 @@
 import React from 'react'
 import WomenImg from '../../images/woman1.png';
 import Navbar from '../../components/Navbar';
-import jsImage from '../../images/js.svg';
-import tsImage from '../../images/ts.svg';
-import muImage from '../../images/MUI.svg'
-import BImage from '../../images/bootstrap.svg';
-import sqlImage from '../../images/mysql.svg';
-import mongoImage from '../../images/mongodb.svg';
-import RImage from '../../images/react.svg';
-import tailwindImage from '../../images/tailwindcss.svg'
-import PyImage from '../../images/python.svg';
-import htmlImage from '../../images/html.svg';
-import cssImage from '../../images/css.svg';
-import NImage from '../../images/nextjs.svg';
 import './style.css';
 import gitImage from '../../images/github.svg';
 
@@ -42,48 +30,15 @@ const Home = () => {
     </div>
     </div>
     <div class='section3'>
-    <div class="mt-[20px] pt-[10px] rounded-xl items-center space-x-4 font-bold  md:h-[330px] h-[100px] w-[290px] md:w-[480px] text-white bg-[#111]">
-    <h1 class='align-middle  md:mx-[170px] mt-[20px] text-white text-1xl cursor-pointer transition ease-in-out delay-150 hover:text-yellow-600'>⭐SKILLS
-      </h1>
-      <ul class='mt-[10px] md:mt-[20px] cursor-pointer transition ease-in-out delay-150 hover:text-yellow-600'>
-        <li class='flex mx-10'>
-        <img class='mx-2' src={jsImage} alt="js" />JAVASCRIPT
-        </li>
-        <li class='flex mx-10'>
-        <img class='mx-2' src={tsImage} alt="ts" />TYPESCRIPT
-        </li>
-        <li class='flex mx-10'>
-        <img class='mx-2' src={muImage} alt="material-ui" />MATERIAL UI
-        </li>
-        <li class='flex mx-10'>
-        <img class='mx-2' src={BImage} alt="bootstrap" />BOOTSTRAP
-        </li>
-        <li class='flex mx-10'>
-        <img class='mx-2' src={tailwindImage} alt="tailwind" />TAILWINDCSS
-        </li>
-        <li class='flex mx-10'>
-        <img class='mx-2' src={mongoImage} alt="mongo db" />MONGO.db
-        </li>
-        <li class='flex mx-10'>
-        <img class='mx-2' src={sqlImage} alt="my sql" />MYSQL
-        </li>
-        <li class='flex mx-10'>
-        <img class='mx-2' src={RImage} alt="react" />React
-        </li>
-       <li class='flex ml-[255px] mt-[-193px]'>
-        <img class='mx-2' src={PyImage} alt="python" />PYTHON
-        </li>
-        <li class='flex ml-[255px]'>
-        <img class='mx-2' src={NImage} alt="next-js" />NEXT.js
-        </li>
-        <li class='flex ml-[255px] '>
-        <img class='mx-2' src={htmlImage} alt="html" />HTML
-        </li>
-        <li class='flex ml-[255px]'>
-        <img class='mx-2' src={cssImage} alt="css" />CSS
-        </li>
-       </ul>
-     
+    <div class="mt-[20px] pt-[10px] rounded-xl items-center space-x-4 font-bold  md:h-[330px] h-[100px] w-[290px] md:w-[480px] bg-[#111] ">
+   <div class='solar-system'>
+   <div class='sun justify-center align-middle text-center'>
+    <div class='earth'>
+    <div class='moon'>
+    </div>
+    </div>
+    </div>
+   </div>
     </div> 
     <div class="mt-[20px] pt-[80px] rounded-xl items-center md:mx-[5px] space-x-4 font-bold h-[100px] md:h-[330px] w-[290px] md:w-[480px] text-white bg-[#111] section4">
     
