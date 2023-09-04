8db9d7d9892d6fe80a291edcd905d440568264e9
kzmfhm <khuzaimanazik4@gmail.com>
2023-09-04 18:06:04 +0500

    Reset code

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -13,15 +13,18 @@ const Home = () => {
     <div class='font-bold text-yellow-700 md:flex'>
       <img src={WomenImg}/>
      </div>
+     <a href={githubLink} target="_blank" rel="noopener noreferrer">
+              <img src={gitImage} alt="github-code" className="gitIcon" />
+            </a>
     <div class='section1'>
+
     <div class="md:mt-[20px] font-sans hover:font-sans p-10 rounded-xl md:pt-[100px] items-center space-x-4 h-[100px] w-[290px] font-bold md:h-[330px] md:w-[480px] text-white section1">
+  
     <p class='md:mx-[40px] font-sans hover:font-sans text-2xl cursor-pointer transition ease-in-out delay-150 hover:text-yellow-700  hover:-translate-y-1 hover:scale-110 duration-300 ...'>💫HI THERE! I'M
       </p>
       <p class='md:mx-[20px] font-sans hover:font-sans pt-[10px] text-5xl cursor-pointer transition ease-in-out delay-150 hover:text-yellow-700  hover:-translate-y-1 hover:scale-110 duration-300 ...'>KHUZAIMA</p>
       <p class='md:mx-[100px] font-sans hover:font-sans pt-[10px] text-2xl cursor-pointer transition ease-in-out delay-150 hover:text-yellow-700  hover:-translate-y-1 hover:scale-110  duration-300 ...'>FRONTEND DEVELOPER.</p>
-      <a href={githubLink} target="_blank" rel="noopener noreferrer">
-              <img src={gitImage} alt="github-code" className="gitIcon" />
-            </a>
+     
     </div>
     <div class='mt-[20px] pt-[110px] align-middle justify-center rounded-xl items-center space-x-4 md:mx-[20px]  font-bold h-[100px] md:h-[330px] w-[290px] md:w-[480px] text-white bg-[#111] section2'>
     <h2 class='align-middle w-[350px] md:mx-[60px] mt-[10px] text-white text-1xl cursor-pointer transition ease-in-out delay-150 hover:text-yellow-600 hover:-translate-y-1 hover:scale-110 duration-300 ..'>
