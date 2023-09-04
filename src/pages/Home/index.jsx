1b744385a4ee2528b083f78438d11652cd1758e3
kzmfhm <khuzaimanazik4@gmail.com>
2023-09-04 22:36:38 +0500

    Updated Styling

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -14,9 +14,7 @@ const Home = () => {
     <img src={WomenImg} alt="Woman Image" width={"520px"} height={"700px"} />
     </div>
     <div class='section1'>
-    <a href={githubLink} target="_blank" rel="noopener noreferrer" class="gitIcon">
-      <img src={gitImage} alt="GitHub Icon" />
-    </a>
+  
     <div class="md:mt-[20px] font-sans hover:font-sans  md:mx-[10px] md:p-12 rounded-xl md:bg-[#111] md:pt-[100px] md:items-center md:space-x-4 h-[80px] w-[290px] font-bold md:h-[330px] md:w-[480px] text-white">
     <h1 class='md:mx-[80px] font-sans hover:font-sans text-2xl cursor-pointer transition ease-in-out delay-150 hover:text-yellow-700  hover:-translate-y-1 hover:scale-110 duration-300 ...'>💫HI THERE! I'M
       </h1>
@@ -30,7 +28,7 @@ const Home = () => {
   </div>
   </div>
     <div class='section3'>
-    <div class="mt-[20px] pt-[10px] rounded-xl items-center space-x-4 font-bold md:mx-[5px] md:h-[330px] h-[100px] w-[290px] md:w-[480px] bg-[#111] ">
+    <div class="mt-[20px] pt-[10px] rounded-xl items-center space-x-4 font-bold md:mx-[5px] md:h-[330px] ml-10 h-[300px] w-[310px] md:w-[480px] bg-[#111] ">
    <div class='solar-system'>
    <div class='sun justify-center align-middle text-center'>
     <div class='earth'>
@@ -40,8 +38,8 @@ const Home = () => {
     </div>
    </div>
     </div> 
-    <div class="mt-[20px] pt-[110px] align-middle justify-center rounded-xl items-center md:mx-[5px] space-x-4 font-bold h-[100px] md:h-[330px] w-[290px] md:w-[480px] text-white bg-[#111] section4">
-    <h2 class='align-middle w-[350px] md:mx-[60px] mt-[10px] text-white text-1xl cursor-pointer transition ease-in-out delay-150 hover:text-yellow-600  hover:-translate-y-1 hover:scale-110 duration-300 ...'>
+    <div class="mt-[20px] md:pt-[110px] align-middle justify-center rounded-xl items-center md:mx-[5px] space-x-4 font-bold h-[300px] ml-10 md:h-[330px] w-[310px] md:w-[480px] text-white bg-[#111] section4">
+    <h2 class='align-middle w-[350px] md:mx-[60px] md:mt-[10px] text-white text-1xl cursor-pointer transition ease-in-out delay-150 hover:text-yellow-600  hover:-translate-y-1 hover:scale-110 duration-300 ...'>
     💫This project is built using React, JavaScript, Material UI,HTML, TailwindCSS and CSS.
       you can also see this project on my Github profile:</h2><br/>
       <a href={githubLink} target="_blank" rel="noopener noreferrer">
