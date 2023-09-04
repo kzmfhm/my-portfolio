6507b7d187104a5fa686241e1fd7ddf305c1b34e
kzmfhm <khuzaimanazik4@gmail.com>
2023-09-04 22:18:11 +0500

    Adding Profile in portfolio

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import WomenImg from '../../images/woman1.png';
+import WomenImg from '../../images/girl1.png';
 import Navbar from '../../components/Navbar';
 import './style.css';
 import gitImage from '../../images/github.svg';
@@ -11,27 +11,24 @@ const Home = () => {
     <Navbar/>
     <header class='h-auto md:pt-[-10px] md:flex flex-wrap overflow-hidden'>
   <div class='font-bold text-yellow-700 md:flex relative woman-img'>
-    <img src={WomenImg} alt="Woman Image" />
-    
-    
-  </div>
+    <img src={WomenImg} alt="Woman Image" width={"520px"} height={"700px"} />
+    </div>
     <div class='section1'>
     <a href={githubLink} target="_blank" rel="noopener noreferrer" class="gitIcon">
       <img src={gitImage} alt="GitHub Icon" />
     </a>
-    <div class="md:mt-[20px] font-sans hover:font-sans md:p-10 rounded-xl md:pt-[100px] md:items-center md:space-x-4 h-[80px] w-[290px] font-bold md:h-[330px] md:w-[480px] text-white">
-    <h1 class='md:mx-[40px] font-sans hover:font-sans text-2xl cursor-pointer transition ease-in-out delay-150 hover:text-yellow-700  hover:-translate-y-1 hover:scale-110 duration-300 ...'>💫HI THERE! I'M
+    <div class="md:mt-[20px] font-sans hover:font-sans  md:mx-[10px] md:p-12 rounded-xl md:bg-[#111] md:pt-[100px] md:items-center md:space-x-4 h-[80px] w-[290px] font-bold md:h-[330px] md:w-[480px] text-white">
+    <h1 class='md:mx-[80px] font-sans hover:font-sans text-2xl cursor-pointer transition ease-in-out delay-150 hover:text-yellow-700  hover:-translate-y-1 hover:scale-110 duration-300 ...'>💫HI THERE! I'M
       </h1>
-      <h2 class='md:mx-[20px] font-sans hover:font-sans md:pt-[10px] text-5xl cursor-pointer transition ease-in-out delay-150 hover:text-yellow-700  hover:-translate-y-1 hover:scale-110 duration-300 ...'>KHUZAIMA</h2>
-      <h3 class='md:mx-[100px] font-sans hover:font-sans md:pt-[10px] text-2xl cursor-pointer transition ease-in-out delay-150 hover:text-yellow-700  hover:-translate-y-1 hover:scale-110  duration-300 ...'>FRONTEND DEVELOPER.</h3>
+      <h2 class='md:pl-[50px] font-sans hover:font-sans md:pt-[10px] text-5xl cursor-pointer transition ease-in-out delay-150 hover:text-yellow-700  hover:-translate-y-1 hover:scale-110 duration-300 ...'>KHUZAIMA</h2>
+      <h3 class='md:pl-[49px] font-sans hover:font-sans md:pt-[10px] text-2xl cursor-pointer transition ease-in-out delay-150 hover:text-yellow-700  hover:-translate-y-1 hover:scale-110  duration-300 ...'>FRONTEND DEVELOPER</h3>
      </div>
-    <div class='mt-[20px] pt-[110px] align-middle justify-center rounded-xl items-center space-x-4 md:mx-[20px]  font-bold h-[100px] md:h-[330px] w-[290px] md:w-[480px] text-white bg-[#111] section2'>
+    <div class='mt-[20px]  pt-[110px] align-middle justify-center rounded-xl items-center space-x-4 md:mx-[10px]  font-bold h-[100px] md:h-[330px] w-[290px] md:w-[480px] text-white bg-[#111] section2'>
     <h2 class='align-middle w-[350px] md:mx-[60px] mt-[10px] text-white text-1xl cursor-pointer transition ease-in-out delay-150 hover:text-yellow-600 hover:-translate-y-1 hover:scale-110 duration-300 ..'>
         💫Frontend Developer / JavaScript & TypeScript Expert/. Mostly, I work on creating user interfaces.
     </h2>
-</div>
-
-    </div>
+  </div>
+  </div>
     <div class='section3'>
     <div class="mt-[20px] pt-[10px] rounded-xl items-center space-x-4 font-bold md:mx-[5px] md:h-[330px] h-[100px] w-[290px] md:w-[480px] bg-[#111] ">
    <div class='solar-system'>
