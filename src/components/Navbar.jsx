edc91dc63ee59242269915a555909411fd7e62fe
kzmfhm <khuzaimanazik4@gmail.com>
2023-09-05 10:51:18 +0500

    Set navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,41 +1,73 @@
-import React from 'react'
-import Logo from '../images/logo.svg'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react';
+import Logo from '../images/logo.svg';
+import { Link } from 'react-router-dom';
+
 
 const Navbar = () => {
+  const [showMobileMenu, setShowMobileMenu] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setShowMobileMenu(!showMobileMenu);
+  };
+
+  const closeMobileMenu = () => {
+    setShowMobileMenu(false);
+  };
+
   return (
-   <nav class='w-full h-14 pt-[10px] bg-black-400 flex justify-between items-center md:px-4'>
-        <div class='text-3xl font-bold mt-[20px]  '>
-            <img src={Logo} alt="my-logo" />
-        </div>
-        <ul class='hidden font-bold text-yellow-700 md:flex '>
-            <li class='mx-[20px] text-2xl cursor-pointer transition ease-in-out delay-150 hover:text-white  hover:-translate-y-1 hover:scale-110 hover:bg-black duration-300 ...'>
-            <Link to='/home'>
-          _Hello
-        </Link></li>
-        <li class='mx-[20px] text-2xl cursor-pointer transition ease-in-out delay-150 hover:text-white  hover:-translate-y-1 hover:scale-110 hover:bg-black duration-300 ...'>
-            <Link to='/about'>
-          _About
-        </Link></li>
-            <li class='mx-[20px] text-2xl cursor-pointer transition ease-in-out delay-150 hover:text-white hover:-translate-y-1 hover:scale-110 hover:bg-black duration-300 ...'>
-            <Link to='/projects'>
-          _Projects
-        </Link>
-            </li>
-            <li class='mx-[20px] text-2xl cursor-pointer transition ease-in-out delay-150 hover:text-white hover:-translate-y-1 hover:scale-110 hover:bg-black duration-300 ...'>
-            <Link to="/contact">
-          _Get In Touch
-        </Link>
-            </li>
-        </ul>
-       
-         <div class='md:hidden text-yellow-700'>
-           <a class='text-4xl'href='#'>&#8801;</a> 
-         </div>
+    <nav className={`w-full h-14 pt-[10px] bg-black-400 flex justify-between items-center md:px-4 ${showMobileMenu ? 'mt-[0px]' : ''}`}>
+      <div className="text-3xl font-bold mt-[20px]">
+        <img src={Logo} alt="my-logo" />
+      </div>
+      <div className={`md:hidden text-yellow-700 ${showMobileMenu ? 'hidden' : 'ml-[0px]'}`}>
+        <a className="text-4xl menu-icons" href="#" onClick={toggleMobileMenu}>
+          &#8801;
+        </a>
+      </div>
+      <ul
+        className={`font-bold text-yellow-700 md:flex ${
+          showMobileMenu ? 'flex' : 'hidden'
+        } md:block`}
+      >
+        <li
+          className={`mx-[20px] text-xl cursor-pointer transition ease-in-out delay-150 hover:text-white hover:-translate-y-1 hover:scale-110 hover:bg-black duration-300 ${
+            showMobileMenu ? 'text-sm border border-black' : ''
+          }`}
+        >
+          <Link to="/home" className={`md:text-xl md:hover:text-white md:hover:-translate-y-1 md:hover:scale-110 ${showMobileMenu ? 'md:hover:bg-[#111]' : ''}`} onClick={closeMobileMenu}>
+            _Hello
+          </Link>
+        </li>
+        <li
+          className={`mx-[20px] text-xl cursor-pointer transition ease-in-out delay-150 hover:text-white hover:-translate-y-1 hover:scale-110 hover:bg-black duration-300 ${
+            showMobileMenu ? 'text-sm ml-[0px]' : ''
+          }`}
+        >
+          <Link to="/about" className={`md:text-xl md:hover:text-white md:hover:-translate-y-1 md:hover:scale-110 ${showMobileMenu ? 'md:hover:bg-[#111]' : ''}`} onClick={closeMobileMenu}>
+            _About
+          </Link>
+        </li>
+        <li
+          className={`mx-[20px] text-xl cursor-pointer transition ease-in-out delay-150 hover:text-white hover:-translate-y-1 hover:scale-110 hover:bg-black duration-300 ${
+            showMobileMenu ? 'text-sm ml-[0px] border border-black' : ''
+          }`}
+        >
+          <Link to="/projects" className={`md:text-xl md:hover:text-white md:hover:-translate-y-1 md:hover:scale-110 ${showMobileMenu ? 'md:hover:bg-[#111]' : ''}`} onClick={closeMobileMenu}>
+            _Projects
+          </Link>
+        </li>
+        <li
+          className={`mx-[20px] text-xl cursor-pointer transition ease-in-out delay-150 hover:text-white hover:-translate-y-1 hover:scale-110 hover:bg-black duration-300 ${
+            showMobileMenu ? 'text-sm ml-[0px] ' : ''
+          }`}
+        >
+          <Link to="/contact" className={`md:text-xl md:hover:text-white md:hover:-translate-y-1 md:hover:scale-110 ${showMobileMenu ? 'md:hover:bg-[#111]' : ''}`} onClick={closeMobileMenu}>
+            _Get In Touch
+          </Link>
+        </li>
+      </ul>
     </nav>
-  )
-}
+  );
+};
 
 export default Navbar;
-
-
