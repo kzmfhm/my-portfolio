cf1ef0cd0391def1b8faa1238a186cb862040699
kzmfhm <khuzaimanazik4@gmail.com>
2025-08-30 20:31:13 +0500

    add commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import React from "react";
+
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/index";
 import Contact from "./pages/Contact/index";
 import Projects from "./pages/Projects/index";
 import About from "./pages/About/index";
 import "tailwindcss/tailwind.css";
-
 import "./App.css";
 
 function App() {
