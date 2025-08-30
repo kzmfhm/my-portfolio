d3ef6d6c4c2777b3bf7f34a00237ebd63ca0444f
kzmfhm <khuzaimanazik4@gmail.com>
2025-08-30 20:31:58 +0500

    add commit

--- src/index.js
+++ src/index.js
@@ -6,8 +6,7 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-  
+  <React.StrictMode>  
     <App />
   </React.StrictMode>
 );
